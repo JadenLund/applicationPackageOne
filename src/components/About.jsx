5a337be793c91f617958bc3390045b0d67f68653
JadenLund <lundjaden@gmail.com>
2023-07-20 04:28:26 -0500

    adding grid to center and left align About.jsx text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,17 +12,17 @@ export default function About() {
         unique needs of each customer.
       </p>
       <img src={Line} className=" my-5" />
-      <div className="flex justify-center">
+      <div className="flex justify-center grid grid-cols-3 gap-8">
         {storeBenefitData.map((info, index) => (
           <div key={`${info.title}_${index}`}>
             <img
               className="w-u flex mx-auto rounded-2xl mt-5"
               src={info.icon}
             />
-            <h2 className="text-3xl font-semibold my-5 justify-center flex">
-              {info.title}
-            </h2>
-            <p className="text-xl ">{info.description}</p>
+            <div className="ml-w w-y">
+              <h2 className="text-3xl font-semibold my-5 flex">{info.title}</h2>
+              <p className="text-xl ">{info.description}</p>
+            </div>
           </div>
         ))}
       </div>
