96e1ec7acbd64d62377c25ed17cd1c3d071b4861
JadenLund <lundjaden@gmail.com>
2023-07-20 07:11:42 -0500

    adding in start of product carousel

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,9 +1,23 @@
+import Card from "./Card";
+import { productCarouselData } from "./Information";
+
 export default function Products() {
   return (
     <div>
       <h1 className="text-center text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-fadein to-fadeout">
-        Products
+        Some of The Best
       </h1>
+      <Card>
+        {productCarouselData.map((info, index) => (
+          <div key={`${info.title}_${index}`}>
+            {" "}
+            {console.log(info)}
+            <img src={info.image} />
+            <h2>{info.title}</h2>
+            <p>{info.description}</p>
+          </div>
+        ))}{" "}
+      </Card>
     </div>
   );
 }
