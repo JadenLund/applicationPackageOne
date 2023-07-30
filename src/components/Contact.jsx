47e0a2ffaef371970c2f1d708e29686996b99db3
JadenLund <lundjaden@gmail.com>
2023-07-30 12:41:49 -0500

    mvp design of contact page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,11 +4,11 @@ export default function Contact() {
       <h1 className="text-center text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-fadein to-fadeout">
         Contact Us
       </h1>
-      <div class="w-full max-w-xs">
+      <div class="max-w-lg mx-auto">
         <form class="px-8 pt-6 pb-8 mb-4">
           <div class="mb-4">
             <input
-              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              class="shadow appearance-none border rounded w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               id="email"
               type="text"
               placeholder="Email"
@@ -16,24 +16,24 @@ export default function Contact() {
           </div>
           <div class="mb-4">
             <input
-              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              class="shadow appearance-none border rounded w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               id="subject"
               type="text"
               placeholder="Subject"
             />
           </div>
           <div class="mb-4">
-            <input
-              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            <textarea
+              class="shadow appearance-none border rounded w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline resize-none"
               id="message"
-              type="text"
+              rows="4"
               placeholder="Message"
-            />
+            ></textarea>
           </div>
 
           <div class="flex items-center justify-between">
             <button
-              class=" bg-gradient-to-r from-fadein to-fadeout text-white font-bold py-2 px-4 rounded "
+              class="bg-gradient-to-r from-fadein to-fadeout text-white font-bold py-2 px-4 rounded"
               type="button"
             >
               Submit
