94613f93e0a8b6c60a09e7b8bed30fc22f4f4cfa
JadenLund <lundjaden@gmail.com>
2023-08-06 17:59:38 -0500

    finishing up find us section

--- src/components/FindUs.jsx
+++ src/components/FindUs.jsx
@@ -21,22 +21,31 @@ export default function FindUs() {
 
   return isLoaded ? (
     <div>
-      <a
-        className="no-underline"
-        href="https://www.google.com/maps/place/1234+N+Main+St,+Fort+Worth,+TX+76106/@32.77622,-97.3481253,17z/data=!3m1!4b1!4m6!3m5!1s0x864e76ac887b6e01:0x684b7d0160556e5e!8m2!3d32.77622!4d-97.345545!16s%2Fg%2F11sn_3pxky?entry=ttu"
-        target="_blank"
-      >
-        1234 Main street avenue, Fort Worth, Texas 12345
-      </a>
-      <GoogleMap
-        mapContainerStyle={{ width: "800px", height: "400px" }}
-        center={coordinates}
-        zoom={14}
-        onLoad={onLoad}
-        onUnmount={onUnmount}
-      >
-        <MarkerF position={coordinates} />
-      </GoogleMap>
+      <h1 className="text-center text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-fadein to-fadeout">
+        Find Us
+      </h1>
+      <div className="flex justify-center text-center text-gray-300 text-xl my-5">
+        <div>
+          <a
+            className="no-underline"
+            href="https://www.google.com/maps/place/1234+N+Main+St,+Fort+Worth,+TX+76106/@32.77622,-97.3481253,17z/data=!3m1!4b1!4m6!3m5!1s0x864e76ac887b6e01:0x684b7d0160556e5e!8m2!3d32.77622!4d-97.345545!16s%2Fg%2F11sn_3pxky?entry=ttu"
+            target="_blank"
+          >
+            1234 Main street avenue, Fort Worth, Texas 12345
+          </a>
+          <div className="rounded-3xl w-r overflow-hidden  my-3">
+            <GoogleMap
+              mapContainerStyle={{ width: "100%", height: "500px" }}
+              center={coordinates}
+              zoom={14}
+              onLoad={onLoad}
+              onUnmount={onUnmount}
+            >
+              <MarkerF position={coordinates} />
+            </GoogleMap>
+          </div>
+        </div>
+      </div>
     </div>
   ) : (
     <>Map could not load</>
