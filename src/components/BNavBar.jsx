d71829570ebfefbc5b03e5a5f60a2e125fff03ba
JadenLund <lundjaden@gmail.com>
2023-08-09 19:44:13 -0500

    removing top nav Home and using brand instead

--- src/components/BNavBar.jsx
+++ src/components/BNavBar.jsx
@@ -1,9 +1,9 @@
 import { Link } from "react-scroll";
 export default function BNavBar() {
   return (
-    <div>
-      <h1 className="text-3xl">Brand</h1>
-      <div className=" space-x-4 text-md">
+    <div className="text-center text-gray-300">
+      <h1 className="text-7xl">Brand</h1>
+      <div className="my-4 space-x-4 text-xl">
         {["Home", "About", "Products", "Find Us", "Contact Us"].map((title) => (
           <Link
             key={title}
@@ -17,12 +17,12 @@ export default function BNavBar() {
           </Link>
         ))}
       </div>
-      <div className="gap-6 flex fa-2x">
+      <div className="justify-center flex gap-6 flex fa-2x ">
         <i className="fa-brands fa-twitter" />
         <i className="fa-brands fa-instagram" />
         <i className="fa-brands fa-facebook" />
       </div>
-      <p class="text-center text-gray-500 text-xs">
+      <p class="mt-4 text-center text-gray-500 text-md">
         &copy;2023 Brand Corp. All rights reserved.
       </p>
     </div>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,26 +4,30 @@ export default function NavBar() {
   return (
     <nav className="sticky z-10 top-0 align-center justify-center flex font-semibold text-xl bg-tab py-5">
       <div className="text-gray-400 flex  w-r justify-between px-8">
-        <div className="flex gap-2">
+        <Link
+          to="Home"
+          spy={true}
+          smooth={true}
+          offset={-80}
+          className="flex gap-2"
+        >
           <i class="fa-regular fa-circle fa-2x" />
           <h1 className="text-3xl"> BRAND</h1>
-        </div>
+        </Link>
 
         <div className=" space-x-4 text-2xl">
-          {["Home", "About", "Products", "Find Us", "Contact Us"].map(
-            (title) => (
-              <Link
-                key={title}
-                to={title}
-                spy={true}
-                smooth={true}
-                offset={-80}
-                className="py-7 px-4 hover:bg-tab-dark cursor-pointer transition duration-700"
-              >
-                {title}
-              </Link>
-            )
-          )}
+          {["About", "Products", "Find Us", "Contact Us"].map((title) => (
+            <Link
+              key={title}
+              to={title}
+              spy={true}
+              smooth={true}
+              offset={-80}
+              className="py-7 px-4 hover:bg-tab-dark cursor-pointer transition duration-700"
+            >
+              {title}
+            </Link>
+          ))}
         </div>
         <div className="gap-6 flex fa-2x">
           <i className="fa-brands fa-twitter" />
